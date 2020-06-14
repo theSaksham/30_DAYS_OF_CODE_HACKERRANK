6fec6d4241cf298186d014625b3c920358a5f52b
theSaksham <50139661+theSaksham@users.noreply.github.com>
2020-06-14 15:07:42 +0530

    Day 10: Binary Numbers

---
+++ Day10.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    result = 0
+    max1 = 0
+
+    while n > 0:
+        if n % 2 == 1:
+            result += 1
+            if result > max1:
+                max1 = result
+
+        else:
+            result = 0
+
+        n //= 2
+
+    print(max1)
