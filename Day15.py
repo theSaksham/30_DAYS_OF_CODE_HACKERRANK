c4dca331c6d130b86fa171a3c1dcab0d93c3556d
theSaksham <50139661+theSaksham@users.noreply.github.com>
2020-07-31 19:10:58 +0530

    Day 15 : Linked List

---
+++ Day15.py
@@ -0,0 +1,10 @@
+    def insert(self,head,data): 
+    #Complete this method
+        if head is None:
+            head = Node(data)
+        else:
+            curr = head
+            while curr.next:
+                curr = curr.next
+            curr.next = Node(data)
+        return head
