b45cd7433621b20bb4f0dd43f90c43a147300bfc
theSaksham <50139661+theSaksham@users.noreply.github.com>
2020-07-31 19:08:22 +0530

    Day 13 : Abstract Classes

---
+++ Day13.py
@@ -0,0 +1,10 @@
+#Write MyBook class
+
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        super().__init__(title, author)
+        self.price = price
+
+    def display(self):
+        print("Title: " + self.title + "\nAuthor: " + self.author + "\nPrice: " + str(self.price))
+
